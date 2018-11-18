ad592ceec5b33054b3ea387f985e107668dc95e9
kingsj0405 <kingsj0405@naver.com>
2018-11-18 18:54:41 +0900

    Implement test csvfile

--- Application/src/main/java/com/example/android/wifirttscan/DeveloperActivity.java
+++ Application/src/main/java/com/example/android/wifirttscan/DeveloperActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DeveloperActivity extends AppCompatActivity {
+    private static final String TAG = "DeveloperActivity";
+
+    private CsvManager mCsvManager;
+
+    private EditText testCsvEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +25,22 @@ public class DeveloperActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mCsvManager = new CsvManager("test.csv");
+
+        testCsvEditText = (EditText)findViewById(R.id.test_csv_edittext);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         return true;
+    }
+
+    public void onClickWriteCsv(View view) {
+        Log.d(TAG, "onClickDeveloperConsole");
+
+        String data = testCsvEditText.getText().toString();
+        mCsvManager.Write(data);
     }
 }
